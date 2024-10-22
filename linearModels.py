c7a55f875a04f57c267ce350da3973b95a9af026
sg670w <sg670w@att.com>
2024-10-22 11:18:00 -0500

    bayes reg

--- linearModels.py
+++ linearModels.py
@@ -1,8 +1,10 @@
 # Importing the required libraries
 from git import Object
 from git.repo import Repo
+from networkx import sigma
 import numpy as np
 from math import log, floor, ceil
+from scipy import linalg
 
 class Utility(object):
     """
@@ -10,8 +12,6 @@ class Utility(object):
     """
     pass
 
-
-import numpy as np
 
 class OrdinaryLeastSquares(object):
     """
@@ -64,7 +64,7 @@ class Ridge(object):
     """
     Ridge Regression Class
     """
-    def __init__(self, alpha=1.0, fit_intercept=True, max_iter=1000, tol=1e-4):
+    def __init__(self, alpha=1.0, fit_intercept=True, max_iter=10000, tol=1e-4):
         self.alpha = alpha
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
@@ -82,7 +82,7 @@ class Ridge(object):
         n_samples, n_features = X.shape                         # Get the number of samples and features
         self.coef_ = np.zeros(n_features)                       # Initialize the coefficients to zeros
         
-        for iteration in range(self.max_iter):                  # For each iteration
+        for _ in range(self.max_iter):                  # For each iteration
             coef_old = self.coef_.copy()                        # Copy the coefficients
             
             for j in range(n_features):                         # For each feature
@@ -130,7 +130,7 @@ class Lasso(object):
     """
     Lasso Regression Class
     """
-    def __init__(self, alpha=1.0, fit_intercept=True, max_iter=1000, tol=1e-4):
+    def __init__(self, alpha=1.0, fit_intercept=True, max_iter=10000, tol=1e-4):
         self.alpha = alpha
         self.fit_intercept = fit_intercept
         self.max_iter = max_iter
@@ -148,7 +148,7 @@ class Lasso(object):
         n_samples, n_features = X.shape                     # Get the number of samples and features
         self.coef_ = np.zeros(n_features)                   # Initialize the coefficients to zeros
         
-        for iteration in range(self.max_iter):              # For each iteration
+        for _ in range(self.max_iter):                      # For each iteration
             coef_old = self.coef_.copy()                    # Copy the coefficients
             
             for j in range(n_features):                     # For each feature
@@ -191,73 +191,218 @@ class Lasso(object):
             formula = f"{self.intercept_:.2f} + " + formula                     # Add the intercept to the formula
         return f"y = {formula}"
 
+class Bayesian(Object):
+    """
+    Bayesian Regression Class
+    y = 
+    
+    Args:
+        max_iter: int, default=300
+            The maximum number of iterations to perform.
+        tol: float, default=0.001
+            The convergence threshold. The algorithm will stop if the coefficients change less than the threshold.
+        alpha_1: float, default=1e-06
+            The shape parameter for the prior on the weights.
+        alpha_2: float, default=1e-06
+            The scale parameter for the prior on the weights.
+        lambda_1: float, default=1e-06
+            The shape parameter for the prior on the noise.
+        lambda_2: float, default=1e-06
+            The scale parameter for the prior on the noise.        
+    """
+    def __init__(self, max_iter=300, tol=0.001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, fit_intercept = None):
+        self.max_iter = max_iter
+        self.tol = tol
+        self.alpha_1 = alpha_1
+        self.alpha_2 = alpha_2
+        self.lambda_1 = lambda_1
+        self.lambda_2 = lambda_2
+        self.fit_intercept = fit_intercept
+    
+        self.intercept_ = None
+        self.coef_ = None
+        
+    def fit(self, X, y):
+        """
+        Fit the model to the data.
+        """
+        # Initialization of the values of the parameters
+        eps = np.finfo(np.float64).eps      # Machine epsilon, the smallest number that can be added to 1.0 to get a larger number
 
-from sklearn.datasets import make_regression
+        # alpha_ is the precision of the weights, lambda_ is the precision of the noise
+        alpha_ = 1.0 / (np.var(y) + eps)    # Add `eps` in the denominator to omit division by zero if `np.var(y)` is zero  
+        lambda_ = 1.0                       # Initialize the noise precision to 1.0
+        
+        lambda_1 = self.lambda_1
+        lambda_2 = self.lambda_2
+        alpha_1 = self.alpha_1
+        alpha_2 = self.alpha_2
+        
+        # Variables to store the values of the parameters from the previous iteration
+        self.scores_ = list()
+        coef_old_ = None
+        
+        XT_y = np.dot(X.T, y)                           # Compute X^T * y
+        U, S, Vh = linalg.svd(X, full_matrices=False)   # Compute the Singular Value Decomposition of X, X = U * S * Vh
+        eigen_vals_ = S**2                              # Compute the eigenvalues of X
+        
+        # Main loop for the algorithm
+        for iter in range(self.max_iter):
+            # Update the coefficients
+            # coef_ formula: coef_ = Vh * (S^2 / (S^2 + lambda_ / alpha_)) * U^T * y
+            coef_ = np.linalg.multi_dot([Vh.T, Vh / (eigen_vals_ + lambda_ / alpha_)[:, np.newaxis], XT_y])
+            
+            # Update alpha and lambda according to (MacKay, 1992)
+            gamma_ = np.sum(1 - alpha_ * eigen_vals_) / np.sum(coef_ ** 2)                      # Compute gamma
+            lambda_ = (gamma_ + 2 * lambda_1 - 1) / (np.sum(coef_ ** 2) + 2 * lambda_2)         # Update lambda
+            alpha_ = (X.shape[0] - gamma_ + 2 * alpha_1 - 1) / (np.sum(y ** 2) + 2 * alpha_2)   # Update alpha
+            
+            # Check for convergence
+            if coef_old_ is not None and np.sum(np.abs(coef_ - coef_old_)) < self.tol:
+                print(f"Converged in {iter} iterations.")
+                break
+            coef_old_ = np.copy(coef_)  # Copy the coefficients
+        
+        self.n_iter_ = iter + 1 
+        self.coef_ = coef_
+        self.alpha_ = alpha_
+        self.lambda_ = lambda_
 
-X, y = make_regression(n_samples=1000, n_features=1, noise=20, random_state=42)
-to_predict = np.array([[1]])
+        # posterior covariance is given by 1/alpha_ * scaled_sigma_
+        self.sigma_ = np.linalg.inv(np.dot(X.T, X) + lambda_ / alpha_ * np.eye(X.shape[1]))
+        
+        if self.fit_intercept:                              # If fit_intercept is True
+            self.intercept_ = self.coef_[0]                 # Set the intercept to the first element of the coefficients
+        
+        return self
+    
+    def predict(self, X):
+        """
+        Predict using the linear model
+        """
+        return np.dot(X, self.coef_)
+    
+    def get_formula(self):
+        """
+        Returns the formula of the model
+        """
+        terms = [f"{coef:.4f} * x_{i}" for i, coef in enumerate(self.coef_)]    # Create the terms of the formula
+        formula = " + ".join(terms)                                             # Join the terms with " + "
+        if self.fit_intercept:                                                  # If fit_intercept is True
+            formula = f"{self.intercept_:.2f} + " + formula                     # Add the intercept to the formula
+        return f"y = {formula}"
 
-# Example Usage OLS (Ordinary Least Squares) Regression
-# ----------------------------------------------------------------------------
-reg = OrdinaryLeastSquares(fit_intercept=True)
-reg.fit(X, y)
+if __name__ == "__main__":
 
-print("\nExample Usage OLS (Ordinary Least Squares) Regression")
-print(f"Regression Coefficients: {reg.coef_}")
-print(f"Regression Intercept: {reg.intercept_}")
-print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
-print(f"Regression Formula: {reg.get_formula()}")
+    from sklearn.datasets import make_regression
+    X, y = make_regression(n_samples=1000, n_features=5, noise=20, random_state=42)
+    to_predict = np.array([[1,2,3,4,5]])
 
+    # Example Usage OLS (Ordinary Least Squares) Regression
+    # ----------------------------------------------------------------------------
+    reg = OrdinaryLeastSquares(fit_intercept=True)
+    reg.fit(X, y)
 
-# Example Usage Ridge Regression
-# ----------------------------------------------------------------------------
-reg = Ridge(alpha=0.5, fit_intercept=True)
-reg.fit(X, y)
-
-print("\nExample Usage Ridge Regression")
-print(f"Regression Coefficients: {reg.coef_}")
-print(f"Regression Intercept: {reg.intercept_}")
-print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
-print(f"Regression Formula: {reg.get_formula()}")
+    print("\nExample Usage OLS (Ordinary Least Squares) Regression")
+    print(f"Regression Coefficients: {reg.coef_}")
+    print(f"Regression Intercept: {reg.intercept_}")
+    print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
+    print(f"Regression Formula: {reg.get_formula()}")
 
 
-# Example Usage Lasso Regression
-# ----------------------------------------------------------------------------
-reg = Lasso(alpha=0.1, fit_intercept=True)
-reg.fit(X, y)
+    # Example Usage Ridge Regression
+    # ----------------------------------------------------------------------------
+    reg = Ridge(alpha=0.5, fit_intercept=True)
+    reg.fit(X, y)
 
-print("\nExample Usage Lasso Regression")
-print(f"Regression Coefficients: {reg.coef_}")
-print(f"Regression Intercept: {reg.intercept_}")
-print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
-print(f"Regression Formula: {reg.get_formula()}")
+    print("\nExample Usage Ridge Regression")
+    print(f"Regression Coefficients: {reg.coef_}")
+    print(f"Regression Intercept: {reg.intercept_}")
+    print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
+    print(f"Regression Formula: {reg.get_formula()}")
 
 
+    # Example Usage Lasso Regression
+    # ----------------------------------------------------------------------------
+    reg = Lasso(alpha=0.1, fit_intercept=True)
+    reg.fit(X, y)
 
-import matplotlib.pyplot as plt
+    print("\nExample Usage Lasso Regression")
+    print(f"Regression Coefficients: {reg.coef_}")
+    print(f"Regression Intercept: {reg.intercept_}")
+    print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
+    print(f"Regression Formula: {reg.get_formula()}")
 
-# Plotting the points X and y
-plt.figure(figsize=(10, 6))
-plt.scatter(X[:, 0], y, color='blue', label='Data Points')
+    # Example Usage Lasso Regression
+    # ----------------------------------------------------------------------------
+    reg = Bayesian(max_iter=300, tol=0.0001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, fit_intercept = True)
+    reg.fit(X, y)
 
-# Plotting the OLS regression line
-reg = OrdinaryLeastSquares(fit_intercept=True)
-reg.fit(X, y)
-plt.plot(X, reg.predict(X), color='red', label='OLS Regression Line')
+    print("\nExample Usage Bayesian Regression")
+    print(f"Regression Coefficients: {reg.coef_}")
+    print(f"Regression Intercept: {reg.intercept_}")
+    print(f"Predicted Value for {to_predict}: {reg.predict(to_predict)}")
+    print(f"Regression Formula: {reg.get_formula()}")
+    
+    
 
-# Plotting the Ridge regression line
-reg = Ridge(alpha=0.5, fit_intercept=True)
-reg.fit(X, y)
-plt.plot(X, reg.predict(X), color='green', label='Ridge Regression Line')
+    # Example plot
+    # ----------------------------------------------------------------------------
+    import matplotlib.pyplot as plt
+    X, y = make_regression(n_samples=1000, n_features=1, noise=15, random_state=42)
 
-# Plotting the Lasso regression line
-reg = Lasso(alpha=0.1, fit_intercept=True)
-reg.fit(X, y)
-plt.plot(X, reg.predict(X), color='orange', label='Lasso Regression Line')
+    # Plotting the points X and y
+    plt.figure(figsize=(10, 6))
+    plt.scatter(X[:, 0], y, color='blue', label='Data Points')
 
-# Adding labels and legend
-plt.xlabel('Feature 0')
-plt.ylabel('Target')
-plt.title('Regression Lines')
-plt.legend()
-plt.show()
+    # Plotting the OLS regression line
+    reg = OrdinaryLeastSquares(fit_intercept=True)
+    reg.fit(X, y)
+    plt.plot(X, reg.predict(X), color='red', label='OLS Regression Line')
+
+    # Plotting the Ridge regression line
+    reg = Ridge(alpha=0.5, fit_intercept=True)
+    reg.fit(X, y)
+    plt.plot(X, reg.predict(X), color='green', label='Ridge Regression Line, Alpha=0.5')
+
+    # Plotting the Lasso regression line
+    reg = Lasso(alpha=0.5, fit_intercept=True)
+    reg.fit(X, y)
+    plt.plot(X, reg.predict(X), color='orange', label='Lasso Regression Line, Alpha=0.5')
+    
+    # Plotting the Bayesian regression line
+    reg = Bayesian(max_iter=300, tol=0.0001, alpha_1=1e-06, alpha_2=1e-06, lambda_1=1e-06, lambda_2=1e-06, fit_intercept = True)
+    reg.fit(X, y)
+    plt.plot(X, reg.predict(X), color='purple', label='Bayesian Regression Line')
+
+    # Adding labels and legend
+    plt.xlabel('Feature 0')
+    plt.ylabel('Target')
+    plt.title('Regression Lines')
+    plt.legend()
+    plt.show()
+
+
+    # # Example plot, Ridge regression lines for different alpha values
+    # # ----------------------------------------------------------------------------
+    # # Plotting the points X and y
+    # plt.figure(figsize=(10, 6))
+    # plt.scatter(X[:, 0], y, color='blue', label='Data Points')
+
+    # # Plotting the Ridge regression lines for different alpha values
+    # alphas = [0.1, 0.5, 1.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0]
+    # colors = ['green', 'purple', 'brown', 'pink', 'gray', 'cyan', 'magenta', 'yellow', 'black', 'lime']
+
+    # for alpha, color in zip(alphas, colors):
+    #     reg = Ridge(alpha=alpha, fit_intercept=True)
+    #     reg.fit(X, y)
+    #     plt.plot(X, reg.predict(X), color=color, label=f'Alpha={alpha}')
+
+    # # Adding labels and legend
+    # plt.xlabel('Feature 0')
+    # plt.ylabel('Target')
+    # plt.title('Ridge Regression Lines Different Alpha Values')
+    # plt.legend()
+    # plt.show()
+
+
